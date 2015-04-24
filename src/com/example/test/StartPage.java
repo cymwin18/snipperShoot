dcad77b7fe3c8564dfb3d937a4e2ed1e153633d4
yangming_chen <yangming_chen@htc.com>
2015-04-24 17:18:32 +0800

    Update Hum Vs Hum logic: 1. Starting online logic impl.

--- src/com/example/test/StartPage.java
+++ src/com/example/test/StartPage.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.test.MainActivity;
 import com.example.test.R;
+
+import java.net.UnknownHostException;
 
 /**
  * Created by cymwin18 on 4/22/15.
@@ -21,6 +25,8 @@ public class StartPage extends Activity {
         startActivity(intent);
     }
 
+    // TODO: for Hum vs hum mode, need to create a socket server.
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -30,18 +36,43 @@ public class StartPage extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.startpage);
 
-        Button btnVsCom = (Button) findViewById(R.id.vsCom_btn);
+        final RadioButton btnVsCom = (RadioButton) findViewById(R.id.vsCom_btn);
+        final RadioButton btnVsHum = (RadioButton) findViewById(R.id.vsHum_btn);
+
+        final EditText ipAddr_et = (EditText) findViewById(R.id.ipAddr_et);
+        ipAddr_et.setVisibility(View.INVISIBLE);
+
+        btnVsCom.setChecked(true);
+
         btnVsCom.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sendVsMode("HUMVSCOM");
+                btnVsCom.setChecked(true);
+                btnVsHum.setChecked(false);
             }
         });
 
-        Button btnVsHum = (Button) findViewById(R.id.vsHum_btn);
         btnVsHum.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                sendVsMode("HUMVSHUM");
+                btnVsCom.setChecked(false);
+                btnVsHum.setChecked(true);
+
+                java.net.InetAddress test = null;
+                String ipAddr = Utils.getWIFILocalIpAdress(getApplicationContext());
+                ipAddr_et.setText(ipAddr);
+                ipAddr_et.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button btnStart = (Button) findViewById(R.id.Start_btn);
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                if (btnVsCom.isChecked()) {
+                    sendVsMode("HUMVSCOM");
+                } else {
+                    sendVsMode("HUMVSHUM");
+                }
             }
         });
     }
 }
+
